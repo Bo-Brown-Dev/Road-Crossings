0d6e3a6ea43d62d25c19030d5107592ffb3447d9
Bo-Brown-Dev <116322660+Bo-Brown-Dev@users.noreply.github.com>
2023-06-30 01:08:42 -0400

    Added file to more easily generate test cases without modifying app code each time tests change

--- utils/Baselines/generate_baselines.py
+++ utils/Baselines/generate_baselines.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from app.app import App
 import pickle
+from app.RoadCrossings import get_roads, get_buffers, get_points, get_tracks, find_crossings, create_map, insert_crossings
 
 
 class BaselineGenerator(App):
@@ -12,48 +13,67 @@ class BaselineGenerator(App):
 
     BASELINE_DIR = 'baseline_collection'
 
-    def generate_tracks(self, data):
+    def generate_tracks(self):
 
-        base_tracks = App.get_tracks(App, self.data)
-        base_tracks.to_pickle(os.path.join(self.BASELINE_DIR, 'Tracks_baseline.pickle'))
+        base_tracks = get_tracks(self.data)
+        return next(base_tracks)
 
     def generate_points(self):
-        base_points = self.get_points(App, self.data)
-        base_points.to_pickle(os.path.join(self.BASELINE_DIR, 'Points_baseline.pickle'))
+        base_points = get_points(self.data)
+        return next(base_points)
 
     def generate_buffers(self):
 
-        base_buffers = self.get_buffers(self.data)
+        base_buffers = get_buffers(self.data)
 
-        # Save polygon to disc
-        with open('./Buffers_baseline', "wb") as poly_file:
-            pickle.dump(base_buffers, poly_file, pickle.HIGHEST_PROTOCOL)
+
+        return next(base_buffers)
 
     def generate_roads(self, buffers):
 
-        base_roads = self.get_roads(buffers)
+        base_roads = get_roads(buffers)
         base_roads.to_pickle(os.path.join(self.BASELINE_DIR, 'Roads_baseline.pickle'))
+        return base_roads
 
     def generate_crossing_points(self, roads):
 
-        base_crossings = App.find_crossings(roads, self.get_tracks(App, self.data))
+        base_tracks = get_tracks(self.data)
+        base_crossings = find_crossings(roads, next(base_tracks))
         base_crossings.to_pickle(os.path.join(self.BASELINE_DIR, 'Crossings_baseline.pickle'))
+        return base_crossings
 
     def generate_output(self, data, crossings):
 
-        new_baseline = self.insertCrossings(self, self.get_points(self, self.data), crossings, data)
+        points = get_points(self.data)
+        new_baseline = insert_crossings(next(points), crossings, data)
         pd.to_pickle(new_baseline, os.path.join(self.BASELINE_DIR, 'Output_baseline.pickle'))
 
     def generate_map(self, roads, crossings):
-        m = self.createMap(self.get_tracks(self, self.data), roads, crossings)
+        m = create_map(self.data, roads, crossings)
         m.save(os.path.join(self.BASELINE_DIR, 'Map_baseline.html'))
-    # Need to refactor these before using in baselines
 
 
-    # App.saveGeopackage(App.get_tracks(data))
 
 BASELINE_INPUT = 'input1.pickle'
 
 
 
-generate = BaselineGenerator(BASELINE_INPUT)
+baseline = BaselineGenerator(BASELINE_INPUT)
+
+points = baseline.generate_points()
+points.to_pickle(os.path.join(baseline.BASELINE_DIR, 'Points_baseline.pickle'))
+
+tracks = baseline.generate_tracks()
+tracks.to_pickle(os.path.join(baseline.BASELINE_DIR, 'Tracks_baseline.pickle'))
+
+buffers = baseline.generate_buffers()
+
+# Save polygon to disc
+with open('./baseline_collection/Buffers_baseline.pickle', "wb") as poly_file:
+    pickle.dump(buffers, poly_file, pickle.HIGHEST_PROTOCOL)
+
+roads = baseline.generate_roads(buffers)
+crossings = baseline.generate_crossing_points(roads)
+baseline.generate_output(baseline.data.trajectories[0], crossings, )
+baseline.generate_map(roads, crossings)
+
